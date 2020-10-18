1ac2a2d98ccff40c5a5b33dde78523c4a9e471aa
wacawa <mikkunmitsuaki@gmail.com>
2020-10-18 13:47:14 +0900

    add basic-info colmn

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: {maximum: 100},
                     format: { with: VALID_EMAIL_REGEX }, uniqueness: true
+  validates :department, length: { in: 2..30 }, allow_blank: true
   has_secure_password
   validates :password, presence: true, length: {minimum: 6}, allow_nil: true
 
