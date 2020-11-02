55665b5b42940e7464587d97ddf81efbdadaeb26
wacawa <mikkunmitsuaki@gmail.com>
2020-11-02 12:27:53 +0900

    modalトライ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :destroy, :correct_user]
-  before_action :logged_in_user, only: [:show, :edit, :update, :destroy]
+  before_action :set_user, only: [:show, :edit, :update, :destroy, :edit_basic_info, :update_basic_info, :correct_user]
+  before_action :logged_in_user, only: [:show, :edit, :update, :destroy, :edit_basic_info, :update_basic_info]
   before_action :correct_user, only: [:edit, :update]
-  before_action :admin_user, only: :destroy
+  before_action :admin_user, only: [:destroy, :edit_basic_info, :update_basic_info]
 
   def index
     @users = User.paginate(page: params[:page])
@@ -42,6 +42,12 @@ class UsersController < ApplicationController
     @user.destroy
     flash[:success] = "ユーザー\"#{@user.name}\"を削除しました。"
     redirect_to users_url
+  end
+
+  def edit_basic_info
+  end
+
+  def update_basic_info
   end
 
   private
