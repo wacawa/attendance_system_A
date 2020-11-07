03fdcf5b017b83182fbf8a7cf0fc72e24354fbc1
wacawa <mikkunmitsuaki@gmail.com>
2020-11-07 14:49:56 +0900

    finish add-user-attendances

--- db/schema.rb
+++ db/schema.rb
@@ -34,8 +34,8 @@ ActiveRecord::Schema.define(version: 2020_11_06_100226) do
     t.string "remember_digest"
     t.boolean "admin", default: false
     t.string "department"
-    t.datetime "basic_time", default: "2020-10-30 23:00:00"
-    t.datetime "work_time", default: "2020-10-30 22:30:00"
+    t.datetime "basic_time", default: "2020-11-06 23:00:00"
+    t.datetime "work_time", default: "2020-11-06 22:30:00"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
