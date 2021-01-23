836723d0cf42b3656d3b7d728b9f3edc641692d5
wacawa <mikkunmitsuaki@gmail.com>
2021-01-23 13:03:20 +0900

    add_superior_column_and_dwt_column_to_users

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,12 +4,25 @@ User.create!(name: "zero",
              email: "sample@email.com",
              password: "password",
              password_confirmation: "password",
-             admin: true
+             admin: true,
+             superior: true
              )
 
-49.times do |n|
+24.times do |n|
   name = Faker::Name.name
   email = "sample#{n+1}@email.com"
+  password = "password"
+  User.create!(name: name,
+               email: email,
+               password: password,
+               password_confirmation: password,
+               superior: true
+               )
+end
+
+24.times do |n|
+  name = Faker::Name.name
+  email = "sample#{n+26}@email.com"
   password = "password"
   User.create!(name: name,
                email: email,
