8f3fb4d210ae83603ed1bf65c2bd947a6c06bdef
wacawa <mikkunmitsuaki@gmail.com>
2021-02-12 12:51:25 +0900

    出勤中社員の抜き出しに成功。社員一覧のインポート欄に枠を付けたくなり、苦戦はしたもののこちらも完成。出勤社員ページのレイアウトから始めてくれ。

--- app/helpers/attendances_helper.rb
+++ app/helpers/attendances_helper.rb
@@ -1,17 +1,18 @@
 module AttendancesHelper
-
+  
   def working_or_not(attendance)
-    return attendance.started_at.present? ? attendance.finished_at.nil? : nil
+    if Date.current == attendance.worked_on
+      return attendance.started_at.present? ? attendance.finished_at.nil? : nil
+    end
   end
-
+  
   def attendance_state(attendance, x)
     if Date.current == attendance.worked_on
       return "出勤" if working_or_not(attendance) == nil && x == 0
       return "退勤" if working_or_not(attendance) && x == 1
     end
-    false
   end
-
+  
   def working_times(start, finish)
     format("%.2f", ((finish - start) / 3600.0))
   end
