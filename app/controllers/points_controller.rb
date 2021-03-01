25fdaa7082517a3a71465469ccc444ac71d3e885
wacawa <mikkunmitsuaki@gmail.com>
2021-03-01 12:57:31 +0900

    indexのみで完結できるようにするかどうか考えてた。_pointを用意しなくて済むけど切り分けが難しい。そしてform_withは二つあっても良いのか？

--- app/controllers/points_controller.rb
+++ app/controllers/points_controller.rb
@@ -1,4 +1,5 @@
 class PointsController < ApplicationController
+  before_action :set_point, only: [:destroy, :edit, :update]
 
   def index
     @points = Point.all
@@ -9,7 +10,7 @@ class PointsController < ApplicationController
     @point = Point.new(point_params)
     if @point.save
       flash[:success] = "拠点情報を追加しました。"
-      redirect_to points_path
+      redirect_to points_url
     else
       @in = "in"
       @points = Point.all
@@ -17,24 +18,34 @@ class PointsController < ApplicationController
     end
   end
 
-  def edit
-    index
-    @epid = Point.find(params[:id]).id
-  end
-
   def destroy
-    @point = Point.find(params[:id])
     @point.destroy
     flash[:success] = "拠点情報 \"#{@point.point_name}\" を削除しました。"
     redirect_to points_url
   end
 
+  def edit
+    @epid = Point.find(params[:id]).id
+    @disabled = "disabled"
+  end
+
   def update
-    @point = Point.find(params[:id])
+    if @point.update_attributes(point_params)
+      flash[:success] = "拠点情報を更新しました。"
+      redirect_to points_url
+    else
+      render :edit
+    end
   end
 
   def point_params
     params.require(:point).permit(:point_name, :attendance_type)
   end
+
+  # before_action
+
+    def set_point
+      @point = Point.find(params[:id])
+    end
 
 end
