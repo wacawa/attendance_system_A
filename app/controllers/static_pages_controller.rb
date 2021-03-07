4047f360ac7fc07a4eaaf6d0fd0b5d9f401fe778
wacawa <mikkunmitsuaki@gmail.com>
2021-03-07 09:39:26 +0900

    finish admin_ui

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,12 @@
 class StaticPagesController < ApplicationController
+  
   def top
   end
+
+  def edit_basic_info
+  end
+
+  def update_basic_info
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,13 +9,14 @@ Rails.application.routes.draw do
 
   get 'index_working_users', to: 'users#index_working_users'
 
+  get 'edit_basic_info', to: 'static_pages#edit_basic_info'
+  patch 'update_basic_info', to: 'static_pages#update_basic_info'
+
   resources :points
 
   resources :users do
     collection { post :import }
     member do
-      get 'edit_basic_info'
-      patch 'update_basic_info'
       get 'attendances/edit_one_month'
       patch 'attendances/update_one_month'
     end
