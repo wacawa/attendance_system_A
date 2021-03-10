58760471b36ed820bab34eafd6d8b46bc164e94f
wacawa <mikkunmitsuaki@gmail.com>
2021-03-10 19:54:39 +0900

    年月日変更はsearchを使うことにした。search_fieldsを使うかはハテナ?

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,8 +1,8 @@
 class AttendancesController < ApplicationController
-  before_action :set_user, only: [:edit_one_month, :update_one_month, :attendance_log]
-  before_action :logged_in_user, only: [:update, :edit_one_month, :attendance_log]
-  before_action :admin_or_correct_user, only: [:update, :edit_one_month, :update_one_month]
-  before_action :set_one_month, only: :edit_one_month    
+  before_action :set_user, only: [:edit_one_month, :update_one_month, :attendance_log, :log]
+  before_action :logged_in_user, only: [:update, :edit_one_month, :attendance_log, :log]
+  before_action :admin_or_correct_user, only: [:update, :edit_one_month, :update_one_month, :log]
+  before_action :set_one_month, only: [:edit_one_month, :log]
 
   UPDATE_ERROR_MSG = "勤怠登録をやり直してくだ。"
 
