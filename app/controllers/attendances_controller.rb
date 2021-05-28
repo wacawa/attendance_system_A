aa65ff9cf0b35a06f0a08281c3651a86c03b9830
wacawa <mikkunmitsuaki@gmail.com>
2021-05-28 14:58:49 +0900

    attendances_edit_requestページの作成へ。gitのbranchも変えといた。merge忘れずに。

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,11 +1,10 @@
 class AttendancesController < ApplicationController
-  before_action :set_user, only: [:edit_one_month, :update_one_month, :attendance_log, :log,
-                                  :before_approval, :superior_request, :update_superior_request,
+  before_action :set_user, only: [:edit_one_month, :update_one_month, :superior_request, :update_superior_request,
                                   :attendances_edit_request, :update_attendances_edit_request]
   before_action :set_user_for_user_id, only: [:update]
-  before_action :logged_in_user, only: [:update, :edit_one_month, :attendance_log, :log]
-  before_action :admin_or_correct_user, only: [:update, :edit_one_month, :update_one_month, :log]
-  before_action :set_one_month, only: [:edit_one_month, :log, :before_approval]
+  before_action :logged_in_user, only: [:update, :edit_one_month]
+  before_action :admin_or_correct_user, only: [:update, :edit_one_month, :update_one_month]
+  before_action :set_one_month, only: [:edit_one_month]
   
   
   UPDATE_ERROR_MSG = "勤怠登録をやり直してくだ。"
